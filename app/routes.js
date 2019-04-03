047a714265fdd7800ddb1bec87132304d6230804
EveBayram <eve.bayram@faststream.civilservice.gov.uk>
2019-04-03 11:30:31 +0100

    landing page

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const utils = require('./utils')
 
-
-
 router.get('/search', function (req, res) {
 
   res.render('search',
@@ -11,6 +9,10 @@ router.get('/search', function (req, res) {
       proceedings: utils.getProceedings()
     })
 
+})
+
+router.get('/', (req, res) => {
+  res.redirect('/landing')
 })
 
 // Add your routes here - above the module.exports line
