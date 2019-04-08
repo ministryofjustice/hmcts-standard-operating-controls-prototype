a285cef4e2ceea6cfa24babcf95ad62ab9602f9c
Eve Bayram <eve.bayram@faststream.civilservice.gov.uk>
2019-04-08 22:01:53 +0100

    display session data routes.js

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@ const utils = require('./utils')
 
 router.get('/search', function (req, res) {
 
+
   res.render('search',
     {
       proceedings: utils.getProceedings()
@@ -16,5 +17,28 @@ router.get('/', (req, res) => {
 })
 
 // Add your routes here - above the module.exports line
+router.post('/om-email-address', function (req, res) {
+Object.assign(req.session.data,req.body)
+console.log(req.session.data)
+res.redirect('/admin_legal')
+})
+
+router.post('/dm-email-address-1', function (req, res) {
+Object.assign(req.session.data,req.body)
+console.log(req.session.data)
+res.redirect('/confirm_details')
+})
+
+router.post('/dm-email-address-2', function (req, res) {
+Object.assign(req.session.data,req.body)
+console.log(req.session.data)
+res.redirect('/confirm_details')
+})
+
+router.post('/soc-type', function (req, res) {
+Object.assign(req.session.data,req.body)
+console.log(req.session.data)
+res.redirect('/search')
+})
 
 module.exports = router
