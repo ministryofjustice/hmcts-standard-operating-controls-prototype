477ad1f1d9d9ea4b68d857aa9f7d613e708fb6b3
Eve Bayram <eve.bayram@faststream.civilservice.gov.uk>
2019-06-17 15:10:01 +0100

    css and js dm check and submit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,3 +21,12 @@ $(document).ready(function () {
     $(v).height(maxHeight);
   });
 })
+
+// added js
+$('.navbar__list-items a').click(function (e) {
+    // e.preventDefault();
+    var target = $(this);
+    target.parents('.navbar').find('.active').removeClass('active');
+    target.parents('li').addClass('active');
+    // target.parents().find('h1:first').text($(this).text());
+});
