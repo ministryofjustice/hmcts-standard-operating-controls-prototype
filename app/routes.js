64a2dd7658717fe101fd3d4dd0acf75c83bc81a8
Eve Bayram <eve.bayram@faststream.civilservice.gov.uk>
2019-06-24 11:21:22 +0100

    content change to q on check completion

--- app/routes.js
+++ app/routes.js
@@ -111,7 +111,7 @@ router.post('/checks_status_answer', function (req, res) {
   var checksStatus = req.session.data['checks_status']
 
   // Check whether the variable matches a condition
-  if (checksStatus == "yes"){
+  if (checksStatus == "no"){
     // Send user to next page
     res.redirect('/task_list')
   }
